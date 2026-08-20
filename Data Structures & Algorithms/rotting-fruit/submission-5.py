cb2c3c1eefe6e2cf9978b190253232224420eb9f
Oluwatobiloba Sokoya <tobilobasokoya@gmail.com>
2026-08-20 06:58:18 -0500

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        result = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        queue = []
+        visited = set()
+        pathExists = False
+        for row in range(rows):
+            for col in range(cols):
+                if (grid[row][col] == 2) and ((row, col) not in visited):
+                    queue.append((row,col))
+                    visited.add((row,col))
+        while queue:
+            qLen = len(queue)
+            for el in range(qLen):
+                r,c = queue.pop(0)
+                for dr, dc in directions:
+                    if ((0 <= r + dr < rows) and (0 <= c+dc < cols)) and ((r+dr, c+dc)not in visited) and (grid[r+dr][c+dc] == 1):
+                        queue.append((r+dr, c+dc))
+                        visited.add((r+dr, c+dc))
+                        grid[r+dr][c+dc] = 2
+                        pathExists = True
+            if pathExists:
+                result += 1
+                pathExists = False
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    return -1
+
+                    
+        return result
+
+                    
+
+        
